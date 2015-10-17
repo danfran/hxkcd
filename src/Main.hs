6c30124111284b73b410f680ec1c66291bfa1b91
danfran <dfrancesconi12@gmail.com>
2015-10-17 17:41:32 +0100

    Cleaning and removed StateT and ReaderT

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,9 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Control.Applicative
-import qualified Control.Monad.State as S
 import Control.Monad
-import Control.Monad.Reader
-import qualified Data.ByteString.Lazy as B
 import qualified Data.Foldable as F (forM_)
 import Data.Maybe (fromJust)
 import Graphics.UI.WX hiding (Event, newEvent)
@@ -28,20 +23,10 @@ data Navigation = First | Previous | Random | Next | Last deriving (Eq)
 
 type VBitmap = Var (Maybe (WxObject (CGDIObject (CBitmap ()))))
 
-data AppEnv = AppEnv {
-    baseDir :: FilePath
-}
-
 data AppState = AppState {
     lastIndex :: Int,
     index :: Int
 } deriving (Show)
-
-type HXkcd = ReaderT AppEnv (S.StateT AppState IO)
-
-newtype HXkcdApp a = HXkcdApp {
-    runHXkcd :: HXkcd a
-} deriving (Functor, Applicative, Monad, MonadIO, MonadReader AppEnv, S.MonadState AppState)
 
 data Components = Components {
     f :: Frame(),
@@ -52,23 +37,13 @@ data Components = Components {
     vbitmap :: VBitmap
 }
 
---data MenuItems = MenuItems {
---    frs :: MenuItem(),
---    prv :: MenuItem(),
---    rnd :: MenuItem(),
---    nxt :: MenuItem(),
---    lst :: MenuItem()
---}
-
-runAll :: HXkcdApp a -> AppEnv -> AppState -> IO (a, AppState)
-runAll k env = S.runStateT (runReaderT (runHXkcd k) env)
-
 main :: IO ()
 main = start hxkcd
 
 hxkcd :: IO ()
 hxkcd
-  = do f <- frame [ text := "HXKCD" ]
+  = do -- start to build the window
+       f <- frame [ text := "HXKCD" ]
 
        p <- panel f []
 
@@ -103,29 +78,17 @@ hxkcd
 
        set sw [ on paint := onPaint vbitmap ]
 
-       -- start
+       -- init
 
        hd <- getHomeDirectory
-       let env = AppEnv $ hd ++ "/.hxkcd/"
-       let state = AppState { lastIndex = 0, index = 0 }
+       let baseDir = hd ++ "/.hxkcd/"
+
+       lastId <- updateAsLast baseDir
+       let initialState = AppState { lastIndex = lastId, index = lastId }
+
        let components = Components { f, titleContainer, dateContainer, altContainer, sw, vbitmap }
---       let menuItems = MenuItems { frs = first, prv = previous, rnd = random, nxt = next, lst = last }
 
-       lastId <- updateAsLast
-
-
---       runAll (start components menuItems) env state
---  where
---    start :: Components -> MenuItems -> HXkcdApp ()
---    start components m = do
---        env <- ask
---        state <- S.get
---        liftIO $ createDirectoryIfMissing False $ baseDir env
---        lastId <- liftIO updateAsLast
---        liftIO $ runReactiveNetwork components m AppState { index = lastId, lastIndex = lastId }
---
---    runReactiveNetwork :: Components -> MenuItems -> AppState -> IO ()
---    runReactiveNetwork components m initialState = do
+       -- start frp
 
        let networkDescription :: forall t. Frameworks t => Moment t ()
            networkDescription = do
@@ -136,8 +99,8 @@ hxkcd
                nextButton     <- event0 f (menu tbNext)
                lastButton     <- event0 f (menu tbLast)
 
-               fetchLastIndex <- fromPoll updateAsLast -- review this - expensive?
-               fetchRandomIndex <- fromPoll updateAsRandom -- review this - expensive?
+               fetchLastIndex <- fromPoll $ updateAsLast baseDir -- review this - expensive?
+               fetchRandomIndex <- fromPoll $ updateAsRandom baseDir -- review this - expensive?
 
                let doFirst :: AppState -> AppState
                    doFirst state = state { index = 1, lastIndex = lastIndex state }
@@ -156,7 +119,7 @@ hxkcd
 
 
                    menuSelection :: Event t AppState
-                   menuSelection = accumE AppState { index = lastId, lastIndex = lastId } $
+                   menuSelection = accumE initialState $
                                        unions [
                                            doFirst <$ firstButton
                                            , doPrevious <$ previousButton
@@ -171,10 +134,10 @@ hxkcd
                             reactimate $ (ioFunc >=> handler) <$> e1
                             return e2
 
-               fetchFeed2E <- mapIO' fetchFeed menuSelection
+               fetchFeed2E <- mapIO' (fetchFeed baseDir) menuSelection
                let fetchFeed2B = stepper Nothing $ fetchFeed2E
 
-               fetchImage2E <- mapIO' (fetchImage components) fetchFeed2E
+               fetchImage2E <- mapIO' (fetchImage components baseDir) fetchFeed2E
 
                sink dateContainer  [ text :== show <$> (maybe "error" getDate)  <$> fetchFeed2B ]
                sink titleContainer [ text :== show <$> (maybe "error" getTitle) <$> fetchFeed2B ]
@@ -191,59 +154,42 @@ hxkcd
     getRandomNum upperLimit = getStdRandom (randomR (1, upperLimit))
 
     -- bad - should not reuse the lastIndex call but the current AppState
-    updateAsRandom :: IO Int
-    updateAsRandom = updateAsLast >>= \lastIndex -> getStdRandom (randomR (1, lastIndex))
+    updateAsRandom :: String -> IO Int
+    updateAsRandom baseDir = updateAsLast baseDir >>= \lastIndex -> getStdRandom (randomR (1, lastIndex))
 
-    updateAsLast :: IO Int
-    updateAsLast
+    updateAsLast :: String -> IO Int
+    updateAsLast baseDir
         = do r <- downloadFeed $ getUrl 0
              let f = fromJust r
              let num = getNum f
---             getFeedPath num >>= \path -> saveFeed path f
-             saveFeed (getFeedPath num) f
+             saveFeed (getFeedPath baseDir num) f
              return num
 
-    fetchFeed :: AppState -> IO (Maybe Xkcd)
-    fetchFeed state
+    fetchFeed :: String -> AppState -> IO (Maybe Xkcd)
+    fetchFeed baseDir state
          = do let id = index state
+              let fileName = getFeedPath baseDir id
               putStrLn $ "Executed fetch with id " ++ show id
-
---              fileName <- getFeedPath id
-              let fileName = getFeedPath id
               putStrLn $ "Get Feed file " ++ fileName
-
               exists <- doesFileExist fileName
               if exists then loadFeed fileName
                         else downloadFeed (getUrl id) >>= \f -> saveFeed fileName $ fromJust f
 
---     getFeedPath :: Int -> HXkcdApp String
---     getFeedPath id = do env <- ask
---                         return $ baseDir env ++ show id ++ ".metadata.json"
+    getFeedPath :: String -> Int -> String
+    getFeedPath baseDir id = baseDir ++ show id ++ ".metadata.json"
 
-    getFeedPath :: Int -> String
-    getFeedPath id = "/home/daniele/.hxkcd/" ++ show id ++ ".metadata.json"
-
-    fetchImage :: Components -> Maybe Xkcd -> IO (Maybe (Bitmap ()))
-    fetchImage components feed
+    fetchImage :: Components -> String -> Maybe Xkcd -> IO (Maybe (Bitmap ()))
+    fetchImage components baseDir feed
          = do let f = fromJust feed
               let uri = getUri f
---              fileName <- getImagePath (getNum f) uri
-              let fileName = getImagePath (getNum f) uri
-
+              let fileName = getImagePath baseDir (getNum f) uri
               putStrLn ("Imnage saved here: " ++ fileName)
-
               exists <- doesFileExist fileName
               unless exists $ downloadImage uri >>= \i -> void (saveImage fileName $ fromJust i)
-              loadImage fileName -- >>= \i -> displayImage (sw components) (vbitmap components) (fromJust i)
---               displayCachedContent components fileName uri
---               displayMetadata components f
+              loadImage fileName
 
---     getImagePath :: Int -> String -> HXkcdApp String
---     getImagePath id uri = do env <- ask
---                              return $ baseDir env ++ show id ++ "-" ++ getFinalUrlPart uri
-
-    getImagePath :: Int -> String -> String
-    getImagePath id uri = "/home/daniele/.hxkcd/" ++ show id ++ "-" ++ getFinalUrlPart uri
+    getImagePath :: String -> Int -> String -> String
+    getImagePath baseDir id uri = baseDir ++ show id ++ "-" ++ getFinalUrlPart uri
 
     onPaint vbitmap dc viewArea
          = do logNullCreate -- to prevent iCCP warning dialog
